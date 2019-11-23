5d536bda693c40526650cd16286805f06625f871
nmatthews323 <nmatthews323@gmail.com>
2019-11-23 15:21:31 +0000

    Add missing function to source code

--- Scripts/ThesisFundingSourceCode.R
+++ Scripts/ThesisFundingSourceCode.R
@@ -119,34 +119,78 @@ extractAbbreviations <- function(Sponsors, removeSet=c(""),addSet=c()) {
   
 }
 
-
-extractAbbreviations <- function(Sponsors, removeSet=c(""),addSet=c()) {
-  require(ngram)
-  #Extract all bracketted words as candidate abbreviations
-  bracketSet <- sapply(strsplit(Sponsors,"\\(|\\["), function(x) x[2])
-  bracketSet <- bracketSet[!is.na(bracketSet)]
-  bracketSet <- gsub("\\)|\\]","",bracketSet)
-  bracketSet <- wordClean(bracketSet)
-  #Remove any that aren't between 4 and 10 letters in length
-  bracketSet <- bracketSet[lapply(bracketSet,wordcount)==1 & nchar(bracketSet) > 2 & nchar(bracketSet) < 8]
-  #Create summary table
-  bracketSetTable <- as.data.frame(table(bracketSet))
-  #Extract most common
-  commonBracketSet <- as.character(bracketSetTable$bracketSet[bracketSetTable$Freq >10])
+sponsorProcess <- function(Sponsors, abbreviations,problemTerms,manualmerges) {
+  #Extract unique sponsors
+  UniqueSponsors <- wordClean(Sponsors,needUnique = TRUE)
+  #Sort by length
+  UniqueSponsors <- UniqueSponsors[order(nchar(UniqueSponsors))]
+  #Extract candidates for merging
+  candidates <- nchar(UniqueSponsors) > 10 & lapply(UniqueSponsors,wordcount) > 1 & !(UniqueSponsors %in% problemTerms)
+  UniqueSponsorsCandidates <- UniqueSponsors[candidates]
+  sponsorMatches <- lapply(UniqueSponsorsCandidates, function(x) agrep(x,UniqueSponsors, ignore.case = TRUE,max.distance = 0.05))
+  #Make dataframe with list items
+  sponsorMatchesDF <- data.frame(Index = unlist(sponsorMatches),
+                                 Obs = UniqueSponsors[unlist(sponsorMatches)],
+                                 Ref = rep(UniqueSponsors[unlist(lapply(sponsorMatches, "[[", 1))],sapply(sponsorMatches, length)),
+                                 stringsAsFactors = FALSE)
+  uniqueSponsorMatches <- sponsorMatchesDF[!duplicated(sponsorMatchesDF$Index),]
+  #Now add in all the other sponsors not merged
+  allIndices <- 1:length(UniqueSponsors)
+  notIncluded <- allIndices[!(1:length(UniqueSponsors) %in% uniqueSponsorMatches$Index)]
+  sponsorMatchesDF2 <- data.frame(Index = notIncluded,
+                                  Obs = UniqueSponsors[notIncluded],
+                                  Ref = UniqueSponsors[notIncluded],
+                                  stringsAsFactors = FALSE)
+  #Complete reference dataframe through a merge
+  sponsorMatchesComb <- rbind(sponsorMatchesDF2,uniqueSponsorMatches)
+  sponsorMatchesComb <- sponsorMatchesComb[!is.na(sponsorMatchesComb$Obs),]
+  #Add in abbreviations column
+  sponsorMatchesComb$Abbreviation <- rep("none",nrow(sponsorMatchesComb))
+  sponsorMatchesComb$AbbreviationOriginal <- rep("none",nrow(sponsorMatchesComb))
+  sponsorMatchesComb$existingAbbrev <- rep("none",nrow(sponsorMatchesComb))
+  sponsorMatchesComb$existingAbbrevOriginal <- rep("none",nrow(sponsorMatchesComb))
+  #abbreviate selected matches
+  forAbbreviation <- lapply(sponsorMatchesComb$Ref,wordcount) > 2 & lapply(sponsorMatchesComb$Ref,wordcount) < 8
+  forAbbreviationOriginal <- lapply(sponsorMatchesComb$Obs,wordcount) > 2 & lapply(sponsorMatchesComb$Obs,wordcount) < 8
+  existingAbbrevs <- lapply(sponsorMatchesComb$Ref,wordcount)==1 & nchar(sponsorMatchesComb$Ref) > 2 & nchar(sponsorMatchesComb$Ref) < 8
+  existingAbbrevs[is.na(existingAbbrevs)] <- FALSE
+  existingAbbrevsOriginal <- lapply(sponsorMatchesComb$Obs,wordcount)==1 & nchar(sponsorMatchesComb$Obs) > 2 & nchar(sponsorMatchesComb$Obs) < 8 
+  existingAbbrevsOriginal[is.na(existingAbbrevsOriginal)] <- FALSE
+  #Extract common abbreviations
+  sponsorMatchesComb$Abbreviation[forAbbreviation] <- abbreviate(sponsorMatchesComb$Ref[forAbbreviation],strict = TRUE,minlength = 3)
+  sponsorMatchesComb$AbbreviationOriginal[forAbbreviationOriginal] <- abbreviate(sponsorMatchesComb$Obs[forAbbreviationOriginal],strict = TRUE,minlength = 3)
+  sponsorMatchesComb$existingAbbrev[existingAbbrevs] <- sponsorMatchesComb$Ref[existingAbbrevs]
+  sponsorMatchesComb$existingAbbrevOriginal[existingAbbrevsOriginal] <- sponsorMatchesComb$Obs[existingAbbrevsOriginal]
   
-  #Extract single word sponsors of 3-9 characters
-  ExistingSponsorAbbrev <- cleanSponsors[lapply(cleanSponsors,wordcount)==1 & nchar(cleanSponsors) > 2 & nchar(cleanSponsors) < 8]
-  existingAbbrev <- as.data.frame(table(ExistingSponsorAbbrev))
-  commonExistingAbrev <- as.character(existingAbbrev$ExistingSponsorAbbrev[existingAbbrev$Freq >5])
+  #Now test if match to abbreviations fed into function
+  sponsorMatchesComb$abbrevMatch <- rep("none",nrow(sponsorMatchesComb))
+  for(abbrev in abbreviations) {
+    #Assign matches
+    sponsorMatchesComb$abbrevMatch[sponsorMatchesComb$Abbreviation == abbrev |
+                                     sponsorMatchesComb$AbbreviationOriginal == abbrev |
+                                     sponsorMatchesComb$existingAbbrev == abbrev |
+                                     sponsorMatchesComb$existingAbbrevOriginal == abbrev] <- abbrev
+  }
   
-  #Merge the two
-  abbreviations <- unique(c(commonBracketSet,commonExistingAbrev))
+  #Special merges for misspellings (e.g. epsrc)
+  for(ii in 1:length(manualmerges)){
+    sponsorMatchesComb$abbrevMatch[sponsorMatchesComb$abbrevMatch %in% manualmerges[[ii]]] <- names(manualmerges)[ii]
+  }
+  #Edit entries where abbreviations have been found and edit to new entry
+  sponsorMatchesComb$RefNew <- sponsorMatchesComb$Ref
+  for(abbrev in unique(sponsorMatchesComb$abbrevMatch[sponsorMatchesComb$abbrevMatch != "none"])) {
+    abbrevMatch <- sponsorMatchesComb$abbrevMatch == abbrev
+    sponsorMatchesComb$RefNew[abbrevMatch] <- sponsorMatchesComb$Ref[abbrevMatch][which.max(nchar(sponsorMatchesComb$Ref[abbrevMatch]))]
+  }
   
-  #Remove set of false abbreviations
-  abbreviations <- abbreviations[!abbreviations %in% removeSet]
-  #And add any necessary
-  abbreviations <- unique(c(abbreviations, addSet))
-  #Return abbreviations
-  return(abbreviations)
+  #Clean up table
+  sponsorMatchesComb$Ref <- sponsorMatchesComb$RefNew
+  sponsorMatchesFinal <- sponsorMatchesComb[,c("Index","Obs","Ref")]
+  #Add in ID column
+  sponsorMatchesFinal <- sponsorMatchesFinal[order(sponsorMatchesFinal$Ref),]
+  sponsorMatchesFinal$SponsorID <- rep.int(1:length(unique(sponsorMatchesFinal$Ref)), as.numeric(table(sponsorMatchesFinal$Ref)))
   
+  #Return output
+  return(sponsorMatchesFinal)
 }
+
